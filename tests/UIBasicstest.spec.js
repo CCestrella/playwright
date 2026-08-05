170b918bc10288b6a87d82b2e28def1a60778bb4
Christin Estrella <christin.estrella@shecodes.com.au>
2026-08-05 10:18:06 +0800

    locator

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,8 +1,20 @@
 const {test} = require('@playwright/test');
 
 
-test('First Playwright test', function ()
+test('Browser Context Playwright test', async ({browser}) =>
 {
-//playwright code
-
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    console.log(await page.title());
+    //css, xpath
+    page.locator
 });
+
+test('Page Playwright test', async ({page}) =>
+{
+       await page.goto('https://google.com/');
+    console.log(await page.title());
+      await expect(page).toHaveTitle("Google");
+});
+  
